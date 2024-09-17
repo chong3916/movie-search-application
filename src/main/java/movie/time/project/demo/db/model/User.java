c4a5772f753baad14f6cdb7754a04c6df0fc1c4a
Cherie Hong <cherieho@usc.edu>
2024-09-17 13:31:55 -0700

    Procfile for deployment

--- src/main/java/movie/time/project/demo/db/model/User.java
+++ src/main/java/movie/time/project/demo/db/model/User.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Table(name = "users")
+@Table(name = "\"User\"")
 public class User {
 
     //Columns-----------------------------------------------------------
