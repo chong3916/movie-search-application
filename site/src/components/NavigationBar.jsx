067ed5c921df65526093474fcd56734c1e1b5611
Cherie Hong <cherieho@usc.edu>
2025-05-08 14:11:35 -0700

    Styling changes

--- site/src/components/NavigationBar.jsx
+++ site/src/components/NavigationBar.jsx
@@ -33,67 +33,67 @@ const NavigationBar = () => {
     }
 
     return (
-        <div>
+        <div style={{ marginBottom: '1rem' }}>
             {bannerData.message && bannerData.message.length > 0 ?
                 <Box sx={{ width: '100%' }}>
                     <ErrorMessage />
                 </Box> : null}
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar component="nav" position="static">
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Box  sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
-                        <Button
-                            sx={{ pointerEvents: 'auto' }}
-                            onClick={() => handleNavigationClick("/")}>
-                            <Typography variant="h6"
-                                        component="div"
-                                        sx={{
-                                            color: 'white',
-                                            fontFamily: 'monospace',
-                                            fontWeight: 700,
-                                            letterSpacing: '.3rem'
-                                        }}>
-                                Movie Time
-                            </Typography>
-                        </Button>
-                    </Box>
-                    <Box sx={{ display: { xs: 'none', sm: 'contents' } }}>
-                        {!matches ?
-                            <IconButton sx={{p: '10px', pointerEvents: 'auto'}} aria-label="menu" onClick={() => setShowSearchBar(true)}>
-                                <SearchIcon/>
-                            </IconButton> :
-                            <Searchbar/>}
-                        {authData.isLoggedIn ? <Button
-                                onClick={() => handleNavigationClick("/user")}
-                                sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
-                                Watchlist
-                            </Button> : null}
-                        {!authData.isLoggedIn ?
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar component="nav" position="static">
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Box  sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
                             <Button
-                                onClick={() => handleNavigationClick("/login")}
-                                sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
-                                Login
-                            </Button> :
-                            <Button
-                                onClick={() => handleLogoutClick()}
-                                sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
-                                Logout
+                                sx={{ pointerEvents: 'auto' }}
+                                onClick={() => handleNavigationClick("/")}>
+                                <Typography variant="h6"
+                                            component="div"
+                                            sx={{
+                                                color: 'white',
+                                                fontFamily: 'monospace',
+                                                fontWeight: 700,
+                                                letterSpacing: '.3rem'
+                                            }}>
+                                    Movie Time
+                                </Typography>
                             </Button>
-                        }
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
+                        </Box>
+                        <Box sx={{ display: { xs: 'none', sm: 'contents' } }}>
+                            {!matches ?
+                                <IconButton sx={{p: '10px', pointerEvents: 'auto'}} aria-label="menu" onClick={() => setShowSearchBar(true)}>
+                                    <SearchIcon/>
+                                </IconButton> :
+                                <Searchbar/>}
+                            {authData.isLoggedIn ? <Button
+                                    onClick={() => handleNavigationClick("/user")}
+                                    sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
+                                    Watchlist
+                                </Button> : null}
+                            {!authData.isLoggedIn ?
+                                <Button
+                                    onClick={() => handleNavigationClick("/login")}
+                                    sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
+                                    Login
+                                </Button> :
+                                <Button
+                                    onClick={() => handleLogoutClick()}
+                                    sx={{my: 2, color: 'white', pointerEvents: 'auto'}} disableRipple>
+                                    Logout
+                                </Button>
+                            }
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </div>
     );
 };
