adfb14063afe5c058d0488c775d8221602acd4fd
Cherie Hong <cherieho@usc.edu>
2025-05-08 18:41:23 -0700

    Added ratings to trending movies

--- site/src/components/VerticalMovieCard.jsx
+++ site/src/components/VerticalMovieCard.jsx
@@ -1,23 +1,58 @@
-import {Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
+import {Box, Card, CardActions, CardContent, CardMedia, CircularProgress, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
+import PropTypes from "prop-types";
+
+function CircularProgressWithLabel(props) {
+    return (
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+            <CircularProgress variant="determinate" {...props} />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Typography
+                    variant="caption"
+                    component="div"
+                    sx={{ color: 'text.secondary' }}
+                >
+                    {`${Math.round(props.value)}%`}
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+CircularProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate variant.
+     * Value between 0 and 100.
+     * @default 0
+     */
+    value: PropTypes.number.isRequired,
+};
 
 const VerticalMovieCard = ({ movie }) => {
     return (
-        <Card
-            sx={{
-                width: { xs: 120, sm: 150, md: 180, lg: 200 }, // Responsive width
-                flexShrink: 0, // Prevent shrinking in flex container
-            }}
-        >
-            <CardMedia
-                component="img"
-                image={movie.posterPath}
-                alt={movie.title}
-                sx={{
-                    height: { xs: 180, sm: 220, md: 270, lg: 300 }, // Responsive height
-                    objectFit: 'cover',
-                }}
-            />
+        <Card sx={{ width: { xs: 140, sm: 150, md: 160 }, flex: '0 0 auto' }}>
+            <Box sx={{ position: 'relative' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ width: '100%', height: 'auto' }}
+                    image={movie.posterPath}
+                    title={movie.title}
+                />
+                <Box sx={{ position: 'absolute', top: 8, right: 8 }}>
+                    <CircularProgressWithLabel value={movie.voteAverage} />
+                </Box>
+            </Box>
             <CardContent>
                 <Typography variant="subtitle1" noWrap>
                     {movie.title}
