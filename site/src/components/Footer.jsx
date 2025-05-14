09d10c378b5538d8ae8ba30bcfb39b89ed48ce19
Cherie Hong <cherieho@usc.edu>
2025-05-13 20:57:36 -0700

    update gitignore and add github link

--- site/src/components/Footer.jsx
+++ site/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from '@mui/material/Link';
 
 const Footer = () => (
     <div style={{position: "fixed", bottom: 0, left: 0, width: "100%", backgroundColor: "#f5f5f5", padding: "10px", textAlign: "center"}}>
-        ch3916
+        <Link target="_blank" href={"https://github.com/chong3916/movie-search-application"}>chong3916</Link>
     </div>
 )
 
